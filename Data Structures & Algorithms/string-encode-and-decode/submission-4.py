d36cb8448f808a2a4f6c32a08586cc7eada8a11a
Amit Kumar Singh <66426717+Amit-Singh-op@users.noreply.github.com>
2026-08-13 12:58:23 +0530

    Bulk sync: 10 submissions

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,8 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        if len(strs) ==0:
+            return ""
+        return "+".join(strs)
+    def decode(self, s: str) -> List[str]:
+        return s.split("+")
